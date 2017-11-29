78b485dadae5a2cdc8e0553ef41a1e6b2925d0fc
Reena Patil <reena.patil@outlook.com>
2017-11-29 23:56:41 +1100

    list items

---
+++ client/components/example.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ListItems extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li> Item 1 </li>
+          <li> Item 2</li>
+        </ul>
+      </div>
+    );
+  }
+}
--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import ListItems from './components/example';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ListItems />, document.getElementById('example'));
