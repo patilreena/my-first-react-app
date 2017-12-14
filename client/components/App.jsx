12ab2f3d43c233351fa0c359324507123d0186e3
Reena Patil <reena.patil@outlook.com>
2017-12-14 23:09:37 +1100

    constructor method

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ListItems from './List.js';
 export default class Client extends React.Component {
+  constructor() {
+    super();
+    this.name = 'Sara';
+  }
+
   render() {
-    const name = 'sara';
-    const add = 3 + 2;
     return (
       <div>
-        <h1> My name is {name} </h1>
-        <h2> sum is {add} </h2>
+        <h1> My name is {this.name} </h1>
         <ListItems />
       </div>
     );
