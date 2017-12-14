68518f97da4726873ca80659f554c30cbd18ca17
Reena Patil <reena.patil@outlook.com>
2017-12-14 20:15:57 +1100

    anatomy of a components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ListItems from './List.js';
 export default class Client extends React.Component {
   render() {
+    const name = 'sara';
+    const add = 3 + 2;
     return (
       <div>
-        <h1> Its' working </h1>
+        <h1> My name is {name} </h1>
+        <h2> sum is {add} </h2>
         <ListItems />
       </div>
     );
